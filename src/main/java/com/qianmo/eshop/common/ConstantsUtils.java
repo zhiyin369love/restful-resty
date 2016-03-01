b7d80152b4a1c489f5e0e16976d6aa3fc1566863
zhangyang <zhangyang@ahqianmo.com>
2016-03-01 13:36:31 +0800

    买家卖家绑定

--- src/main/java/com/qianmo/eshop/common/ConstantsUtils.java
+++ src/main/java/com/qianmo/eshop/common/ConstantsUtils.java
@@ -25,4 +25,20 @@ public class ConstantsUtils {
 	public final static Integer ORDER_PAYMENT_STATUS_RECEIVED = 2;
 	/**********************************************************/
 
+	/**************************验证码/邀请码类型**************************/
+	/**注册**/
+	public final static Integer INVITE_VERIFY_CODE_TYPE_REGISTER = 1;
+	/**重置密码**/
+	public final static Integer INVITE_VERIFY_CODE_TYPE_RESET = 2;
+	/**邀请**/
+	public final static Integer INVITE_VERIFY_CODE_TYPE_INVITE = 3;
+	/**********************************************************/
+
+	/**************************买家卖家绑定**************************/
+	/**绑定中**/
+	public final static Integer BUYER_SELLER_STATUS_BIDING = 0;
+	/**取消绑定**/
+	public final static Integer BUYER_SELLER_STATUS_BIDING_CANCEL = 1;
+	/**********************************************************/
+
 }
--- src/main/java/com/qianmo/eshop/model/user/invite_verify_code.java
+++ src/main/java/com/qianmo/eshop/model/user/invite_verify_code.java
@@ -2,6 +2,7 @@ package com.qianmo.eshop.model.user;
 
 import cn.dreampie.orm.Model;
 import cn.dreampie.orm.annotation.Table;
+import com.qianmo.eshop.common.yamlRead;
 
 /**
  * Created by ccq on 16-1-1.
@@ -9,4 +10,13 @@ import cn.dreampie.orm.annotation.Table;
 @Table(name = "buyer_receive_address")
 public class invite_verify_code extends Model<invite_verify_code> {
     public final static invite_verify_code dao = new invite_verify_code();
+
+    /**
+     * 根据验证码和类别获取验证信息
+     */
+    public invite_verify_code getInviteByCode(int verifyCode, int type) {
+        String sql  = "";
+        sql = yamlRead.getSQL("getSellerIdByVerifyCode","buyer/buyer");
+        return invite_verify_code.dao.findFirst(sql, verifyCode, type);
+    }
 }
