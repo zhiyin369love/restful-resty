d5f21f5fdf7de9297b6e320784588fb5e56b1f5a
fangxigang <fangxigang@ahqianmo.com>
2016-03-02 19:21:26 +0800

    读取yaml配置文件的工具类

---
+++ src/main/java/com/qianmo/eshop/common/YamlRead.java
@@ -0,0 +1,41 @@
+package com.qianmo.eshop.common;
+
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.FileInputStream;
+import java.net.URL;
+import java.util.Map;
+
+/**
+ * Created by fxg06 on 2016/3/2.
+ */
+public class YamlRead {
+    //从yaml文件中
+    // 根据key获取对应的sql语句
+    public static String getSQL(String key,String filename){
+        String sql = null;
+        try {
+            Yaml yaml = new Yaml();
+            URL url = getURL(filename);
+            if (url != null) {
+                Map<String, Object> data = (Map<String, Object>)yaml.load(new FileInputStream(url.getFile()));
+                sql = data.get(key).toString();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return sql;
+    }
+
+
+    //根据名称获取对应的yaml文件
+    public static URL getURL(String key){
+        URL url = null;
+        try {
+            url = YamlRead.class.getClassLoader().getResource("sqlyaml/" + key + ".yaml");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return url;
+    }
+}
