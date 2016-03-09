841f335e17c757fa3ce8db7c8faece1a83d5d29e
Metro1 <wangshengsheng@ahqianmo.com>
2016-03-09 02:55:37 -0500

    sql命名规范

--- src/main/java/com/qianmo/eshop/resource/buyer/OrderResource.java
+++ src/main/java/com/qianmo/eshop/resource/buyer/OrderResource.java
@@ -36,30 +36,31 @@ public class OrderResource extends BuyerResource {
        try {
 
         //订单实体查询sql
-        String sql3 = YamlRead.getSQL("getFieldOrderInfoAll","buyer/order");
+        String sqlOrderinfo = YamlRead.getSQL("getFieldOrderInfoAll","buyer/order");
         //订单备注列表查询sql
-        String sql4 = YamlRead.getSQL("getFirldOrderRemarkAll","buyer/order");
+        String sqlOrderremark = YamlRead.getSQL("getFirldOrderRemarkAll","buyer/order");
         //商品实体列表查询sql
            List<HashMap> resultMap = getOrderHashMaps(id);
 
         //买家信息实体查询sql
-        HashMap result3 = new HashMap();
-        String sql1_1 = YamlRead.getSQL("getFieldBuyerInfoAll","buyer/order");
+        HashMap result_buyer = new HashMap();
+        String sqlbuyer_info = YamlRead.getSQL("getFieldBuyerInfoAll","buyer/order");
         //买家收货人实体
-        String sql1_2 = YamlRead.getSQL("getFieldBuyerReceiveAll","buyer/order");
+        String sqlbuyer_receive = YamlRead.getSQL("getFieldBuyerReceiveAll","buyer/order");
         order_user o = new order_user();
-        if(order_user.dao.find(sql1_1,id)!=null && order_user.dao.find(sql1_1,id).size()>0){
-         o = order_user.dao.find(sql1_1,id).get(0);
+           List<order_user> order_users_list = order_user.dao.find(sqlbuyer_info,id);
+        if(order_users_list!=null && order_users_list.size()>0){
+         o = order_users_list.get(0);
         }
-        result3.put("buyer_id",o.get("buyer_id"));
-        result3.put("buyer_name",o.get("name"));
-        result3.put("buyer_receive", buyer_receive_address.dao.find(sql1_2,id));
+           result_buyer.put("buyer_id",o.get("buyer_id"));
+           result_buyer.put("buyer_name",o.get("name"));
+           result_buyer.put("buyer_receive", buyer_receive_address.dao.find(sqlbuyer_receive,id));
 
         //返回json
-        result.put("buyer_info",result3);
+        result.put("buyer_info",result_buyer);
         result.put("goods_list",resultMap);
-        result.put("order_info",order_info.dao.find(sql3,id));
-        result.put("order_remark_list", order_remark.dao.find(sql4,id));
+        result.put("order_info",order_info.dao.find(sqlOrderinfo,id));
+        result.put("order_remark_list", order_remark.dao.find(sqlOrderremark,id));
         return result;
        }catch (Exception e) {
         //异常情况，方便记录日志 TODO
@@ -72,25 +73,25 @@ public class OrderResource extends BuyerResource {
     }
 //商品实体封装
     public List<HashMap> getOrderHashMaps(long id) {
-        HashMap result2 =  new HashMap();
+        HashMap resultgoods =  new HashMap();
         //商品信息
-        String sql2_1 = YamlRead.getSQL("getFirldGoodsInfoAll","buyer/order");
+        String sqlgoods_info = YamlRead.getSQL("getFirldGoodsInfoAll","buyer/order");
         //商品规格列表
-        String sql2_2 = YamlRead.getSQL("getFieldGoodsSkuAll","buyer/order");
+        String sqlgoodssku = YamlRead.getSQL("getFieldGoodsSkuAll","buyer/order");
         //商品分类
-        String sql2_3 = YamlRead.getSQL("getFieldGoodsTypeALL","buyer/order");
+        String sqlgoodstype = YamlRead.getSQL("getFieldGoodsTypeALL","buyer/order");
         Map goodsResult = new HashMap();
 
-        List<goods_info>  goods_infoList =  goods_info.dao.find(sql2_1,id);
+        List<goods_info>  goods_infoList =  goods_info.dao.find(sqlgoods_info,id);
         List<HashMap> resultMap = new ArrayList<HashMap>();
         for (goods_info goodlist: goods_infoList ){
-            result2.clear();
-            result2.put("goods_info", goods_info.dao.find(sql2_1,id));
+            resultgoods.clear();
+            resultgoods.put("goods_info", goods_info.dao.find(sqlgoods_info,id));
             long goodsNum = (Long)((JSONObject)goodlist.get("goods_info")).get("id");
             long category_id = (Long)((JSONObject)goodlist.get("goods_info")).get("category_id");
-            result2.put("goods_sku_list", goods_sku.dao.find(sql2_2,goodsNum));
-            result2.put("goods_type", goods_category.dao.find(sql2_3,category_id));
-            resultMap.add(result2);
+            resultgoods.put("goods_sku_list", goods_sku.dao.find(sqlgoodssku,goodsNum));
+            resultgoods.put("goods_type", goods_category.dao.find(sqlgoodstype,category_id));
+            resultMap.add(resultgoods);
         }
         return resultMap;
     }
