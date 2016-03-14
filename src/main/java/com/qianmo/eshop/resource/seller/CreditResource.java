bc4f16459ffb242e33debe357bfc11254df0dc3f
Metro1 <wangshengsheng@ahqianmo.com>
2016-03-14 16:48:30 +0800

    赊账bug修改

--- src/main/java/com/qianmo/eshop/resource/seller/CreditResource.java
+++ src/main/java/com/qianmo/eshop/resource/seller/CreditResource.java
@@ -3,6 +3,7 @@ package com.qianmo.eshop.resource.seller;
 import cn.dreampie.common.http.result.HttpStatus;
 import cn.dreampie.common.http.result.WebResult;
 import cn.dreampie.orm.page.FullPage;
+import cn.dreampie.orm.page.Page;
 import cn.dreampie.route.annotation.API;
 import cn.dreampie.route.annotation.GET;
 import cn.dreampie.route.annotation.PUT;
@@ -22,13 +23,11 @@ import com.qianmo.eshop.model.user.invite_verify_code;
 import com.qianmo.eshop.model.user.user_info;
 import com.qianmo.eshop.resource.buyer.*;
 import com.qianmo.eshop.resource.z_common.ApiResource;
+import com.qianmo.eshop.resource.seller.OrderResource;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-
-import com.qianmo.eshop.resource.buyer.OrderResource;
-
 /**
  * 赊账 和 销账
  * @author wss
@@ -44,7 +43,8 @@ public class CreditResource extends SellerResource {
      * @return
      */
     @GET
-    public HashMap getCredit(int page_start, int page_step, int show_type, Integer status) {
+    public HashMap getCredit(Integer page_start, Integer page_step, int show_type, Integer status) {
+        System.out.print("进来");
         long seller_id = SessionUtil.getUserId();
         //long buyer_id = SessionUtil.getUserId();
         HashMap all = new HashMap();
@@ -57,15 +57,22 @@ public class CreditResource extends SellerResource {
             List<HashMap> resultMapBuyer = new ArrayList<HashMap>();
             String sqlbuyer_info = YamlRead.getSQL("getFieldBuyerInfoAll", "seller/credit");
             //分页
-            FullPage<credit> creditFullPagelist = null;
+            if(page_start == null || page_start ==0) {
+                page_start = ConstantsUtils.DEFAULT_PAGE_START;
+            }
+            if(page_step == null || page_step == 0) {
+                page_step = ConstantsUtils.DEFAULT_PAGE_STEP;
+            }
+            int pageNumber = page_start/page_step + 1;
+            Page<credit> creditFullPagelist = null;
             if (status != null) {
                 sqlbuyer_info = sqlbuyer_info + "and c.status = ?";
                // order_users_list = credit.dao.find(sqlbuyer_info, seller_id, status);
-                creditFullPagelist = credit.dao.fullPaginate(page_start/page_step + 1,page_step,sqlbuyer_info,seller_id,status);
+                creditFullPagelist = credit.dao.paginate(pageNumber,page_step,sqlbuyer_info,seller_id,status);
             } else {
-                creditFullPagelist = credit.dao.fullPaginate(page_start/page_step + 1,page_step,sqlbuyer_info,seller_id); //获取一个卖家对应的所有买家
+                creditFullPagelist = credit.dao.paginate(pageNumber, page_step, sqlbuyer_info, seller_id); //获取一个卖家对应的所有买家
             }
-            if (creditFullPagelist != null)
+                // if (creditFullPagelist != null)
             for (credit credit_list : creditFullPagelist.getList()) {
                 long buyer_id_list = credit_list.get("buyer_id");
                 String users_buyer_name = YamlRead.getSQL("getFieldUserInfoAll", "seller/credit");
@@ -74,22 +81,22 @@ public class CreditResource extends SellerResource {
                 if (order_name_list != null && order_name_list.size() > 0) {
                     o = order_name_list.get(0);
                 }
-                result_buyerinfo.put("name", o.get("name"));
+                result_buyerinfo.put("buyer_nickname", o.get("name"));
                 result_buyerinfo.put("buyer_id", buyer_id_list);
                 resultMapBuyer.add(result_buyerinfo);
 
                 String total_order_count = YamlRead.getSQL("getFirldCountOrderUserAll", "seller/credit");
                 String total_price_count = YamlRead.getSQL("getFirldCountPriceOrderInfoAll", "seller/credit");
                 //订单买家汇总赊账金额
-                order_info order_info_list = new order_info();
-                order_info order_info_count = new order_info();
+              //  order_info order_info_list = new order_info();
+             //   order_info order_info_count = new order_info();
                 order_info credit_order_id = order_info.dao.findFirst(total_price_count, buyer_id_list, seller_id);
                 order_info credit_order_count = order_info.dao.findFirst(total_order_count, buyer_id_list, seller_id);
 
                 String sqlcre = YamlRead.getSQL("getFirldCreditAll", "seller/credit");
                 credit order_name_list_credit = credit.dao.findFirst(sqlcre, seller_id);
-                result_buyer_credit.put("total_order_count", order_info_count.get("num"));
-                result_buyer_credit.put("total_price", order_info_list.get("total_price"));
+                result_buyer_credit.put("total_order_count", credit_order_count.get("num"));
+                result_buyer_credit.put("total_price", credit_order_id.get("total_price"));
                 result_buyer_credit.put("buyer_info", resultMapBuyer);
                 result_buyer_credit.put("credit_id", order_name_list_credit.get("id"));
 
@@ -109,39 +116,50 @@ public class CreditResource extends SellerResource {
             HashMap result_goods_list = new HashMap();
             //List<credit> creditOrderList = new ArrayList<credit>();
             //分页
-            FullPage<credit> creditFullPagelist = null;
+            Page<credit> creditFullPagelist = null;
+            //分页
+            if(page_start == null || page_start ==0) {
+                page_start = ConstantsUtils.DEFAULT_PAGE_START;
+            }
+            if(page_step == null || page_step == 0) {
+                page_step = ConstantsUtils.DEFAULT_PAGE_STEP;
+            }
+            int pageNumber = page_start/page_step + 1;
             String sqlcre = YamlRead.getSQL("getFirldSellerCreditAll", "seller/credit");
             if (status != null) {   //判断订单赊账是否
                 sqlcre = sqlcre + "c.status = ?";
                 //creditOrderList = credit.dao.find(sqlcre, seller_id, status);
-                creditFullPagelist = credit.dao.fullPaginate(page_start/page_step + 1,page_step,sqlcre,seller_id,status);
+                creditFullPagelist = credit.dao.paginate(pageNumber,page_step,sqlcre,seller_id,status);
             } else {
-                creditFullPagelist = credit.dao.fullPaginate(page_start/page_step + 1,page_step,sqlcre,seller_id);
+                creditFullPagelist = credit.dao.paginate(pageNumber,page_step,sqlcre,seller_id);
             }
             if (creditFullPagelist != null){
                 for (credit credit_list : creditFullPagelist.getList()) {
                     //一个订单对应一个赊账
-                    int id = credit_list.get("id");
+                    long id = credit_list.get("id");
                     int seller_status = credit_list.get("status");
-                    int credit_id_list = credit_list.get("order_num");
-                    OrderResource resource = new com.qianmo.eshop.resource.buyer.OrderResource();
-                    List<HashMap> resultMap2 = resource.getOrderHashMaps(credit_id_list);
-                    result_goods_order.put("goods_list", result_goods_list);                             //1
+                    long credit_id_list = credit_list.get("order_num");
+                    order_info orderInfoId = order_info.dao.findFirst("select id from order_info where num = ?",credit_id_list);
+                    long orderId = orderInfoId.get("id");
+                    OrderResource resource = new OrderResource();
+                    List<HashMap> resultMapGood = resource.getOrderHashMaps(orderId);
+                    result_goods_order.put("goods_list", resultMapGood);                             //1
                     //买家信息实体
                     String sql_buyer_info = YamlRead.getSQL("getFieldBuyerInfoAll", "seller/order");
                     String sql_buyer_receive = YamlRead.getSQL("getFieldBuyerReceiveAll", "seller/order");
-                    order_user o = order_user.dao.findFirst(sql_buyer_info, seller_id);
-
-                    result_buyer_info.put("buyer_id", o.get("buyer_id"));
-                    result_buyer_info.put("name", o.get("name"));
+                    order_user o = order_user.dao.findFirst("select distinct c.buyer_id from credit c where c.seller_id = ?", seller_id);
+                    long buyer_id = o.get("buyer_id");
+                    result_buyer_info.put("buyer_id",buyer_id);
+                    user_info buyer_name = user_info.dao.findFirst("select nickname from user_info where id = ? ",buyer_id);
+                    result_buyer_info.put("buyer_nickname", buyer_name.get("nickname"));
                     result_buyer_info.put("buyer_receive", buyer_receive_address.dao.find(sql_buyer_receive, seller_id));
                     result_goods_order.put("buyer_info", result_buyer_info);                        //2
                     //订单实体
-                    String sql3 = YamlRead.getSQL("getFieldOrderInfoAll", "seller/order");
-                    result_goods_order.put("order_info", order_info.dao.find(sql3, seller_id));   //3
+                    String sqlOrderInfo = YamlRead.getSQL("getFieldOrderInfoAll", "seller/order");
+                    result_goods_order.put("order_info", order_info.dao.find(sqlOrderInfo, seller_id));   //3
                     //订单备注
-                    String sql4 = YamlRead.getSQL("getFirldOrderRemarkAll", "seller/order");
-                    result_goods_order.put("order_remark_list", order_remark.dao.find(sql4, seller_id));  //4
+                    String sqlOrderRemark = YamlRead.getSQL("getFirldOrderRemarkAll", "seller/order");
+                    result_goods_order.put("order_remark_list", order_remark.dao.find(sqlOrderRemark, seller_id));  //4
 
                     //多个订单实体
                     result_buyer_credit.put("order", result_goods_order);
@@ -152,7 +170,6 @@ public class CreditResource extends SellerResource {
             }
             all.put("total_count", creditsList.size());
             all.put("credit_list", creditsList);
-
         }
         return all;
     }
