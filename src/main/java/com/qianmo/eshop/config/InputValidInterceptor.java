61020dc12c821df0958570894ecd110ee8d6458a
zhangyang <zhangyang@ahqianmo.com>
2016-03-16 15:18:06 +0800

    修改通用拦截的部分逻辑

--- src/main/java/com/qianmo/eshop/config/InputValidInterceptor.java
+++ src/main/java/com/qianmo/eshop/config/InputValidInterceptor.java
@@ -1,7 +1,6 @@
 package com.qianmo.eshop.config;
 
 
-
 import cn.dreampie.common.http.exception.WebException;
 import cn.dreampie.route.core.Params;
 import cn.dreampie.route.core.RouteInvocation;
@@ -11,7 +10,10 @@ import com.alibaba.druid.util.StringUtils;
 import com.qianmo.eshop.common.ConstantsUtils.RETURN_CODE;
 import com.qianmo.eshop.config.ValidateResult.WARNING_MSG;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -22,25 +24,26 @@ import java.util.regex.Pattern;
 public class InputValidInterceptor implements Interceptor {
 
     /**
-     *对输入参数进行拦截
+     * 对输入参数进行拦截
+     *
      * @param ri
      */
     public void intercept(RouteInvocation ri) {
 
         //类名
-        ValidateResult result;
+        ValidateResult result = new ValidateResult(RETURN_CODE.IS_OK, null);
         String className = ri.getResourceClass().getName();
-        if(!className.startsWith("com.qianmo.eshop.resource")) {
+        if (!className.startsWith("com.qianmo.eshop.resource")) {
             ri.invoke();
         } else {
             //输入参数
             Params params = ri.getRouteMatch().getParams();
             //如果不需要参数
-            if(params == null) {
+            if (params == null) {
                 ri.invoke();
             } else {
                 try {
-                    result = checkParams(ValidateRule.values(),params);
+                    result = checkParams(ValidateRule.values(), params);
                     if (!RETURN_CODE.IS_OK.equals(result.getReturnCode())) {
                         throw new InterceptorException(result.getReturnMessage());
                     } else {
@@ -63,7 +66,6 @@ public class InputValidInterceptor implements Interceptor {
     }
 
 
-
     /**
      * 验证请求的参数个数、顺序、和正则。
      *
@@ -71,79 +73,81 @@ public class InputValidInterceptor implements Interceptor {
      * @param params  前台传入的参数
      * @return
      */
-    public static ValidateResult checkParams(ValidateRule[] sources ,
+    public static ValidateResult checkParams(ValidateRule[] sources,
                                              Params params) {
         if (params == null || sources == null) {// 参数列表或请求参数为空，则默认校验通过
             return new ValidateResult(RETURN_CODE.IS_OK, null);
         }
 
-        // 去除params中的特定参数 TODO
-       /* for (String key : ControlConstants.PARAM_ARRAY) {
-            if (params.containsName(key)) {
-                params.remove(key);
+        // 创建ValidateResult对象,作为校验出参，默认通过
+        ValidateResult result = new ValidateResult(RETURN_CODE.IS_OK, null);
+        //校验规则可能很大，因此将参数做为主循环，减少循环次数
+        for (String key : params.getNames()) {
+            String regexs = "";
+            //TODO 例外参数
+            Object valueObject = params.get(key);//获取前台请求的值
+            for (ValidateRule rule : sources) {
+                if (key.equals(rule.name())) {
+                    regexs = rule.getCheckRegx();//获取参数对应的校验规则，如果不存在校验规则，则不去校验
+                    break;
+                }
             }
-        }*/
-
-        ValidateResult result = new ValidateResult(RETURN_CODE.IS_OK, null);// 创建ValidateResult对象,作为校验出参，默认通过
-        // 按配置规则进行校验
-        for(ValidateRule parameter : sources) {
+            // 按配置规则进行校验
+     /*    for(ValidateRule parameter : sources) {
             if (parameter == null || StringUtils.isEmpty(parameter.getCheckRegx())) {
                 continue;// 如果Parameter为空，或者regex为空，则不校验
             }
             String regexs = parameter.getCheckRegx();// 获取配置的校验规则
             String key = parameter.name();
-            Object valueObject = params.get(key);//获取前台请求的值
-            List<ValidateType> validateTypes = getValidateTypes(key, regexs);
-            String value ;
+            Object valueObject = params.get(key);//获取前台请求的值*/
+            List<ValidateType> validateTypes = getValidateTypes(regexs);
+            String value;
             for (ValidateType validateType : validateTypes) {
-                if (valueObject == null) {
+                if (valueObject == null || StringUtils.isEmpty(valueObject.toString())) {
                     // 如果传入为空，校验值为required，则返回失败
                     if (validateType == ValidateType.required) {
                         return new ValidateResult(RETURN_CODE.SYSTEM_ERROR, getText(validateType.getErrMsg(), key));
-                    }else {
-                        // 传入为空，校验值为非required，默认校验通过(校验值已排序，required顺序为第一个)
-                        break;
                     }
                 }
-                 value = valueObject.toString();
-                    try {
-                        switch (validateType) {
-                            case required:// 必填字段校验
-                                isRequired(value, validateType, key, result);
-                                break;
-                            case length:// 长度校验
-                                validateLength(value, validateType.getRegVal(), validateType, key, result);
-                                break;
-                            case maxlength:// 长度最大值校验
-                                validateMaxlength(value, validateType.getRegVal(), validateType, key, result);
-                                break;
-                            case minlength:// 长度最小值校验
-                                validateMinlength(value, validateType.getRegVal(), validateType, key, result);
-                                break;
-                            case range:// 区间校验
-                                validateRange(value, validateType.getRegVal(), validateType, key, result);
-                                break;
-                            case idcard:// 证件号校验
-                                // 校验规则：15位通过 || 18位通过
-                                validateRegex(value, ValidateType.REGEX.IDCARD_15, validateType, key, result);
-                                if (!RETURN_CODE.IS_OK.equals(result.getReturnCode())) {
-                                    validateRegex(value, ValidateType.REGEX.IDCARD_18, validateType, key, result);
-                                }
-                                break;
-                            case regex:// 正则表达式校验
-                                validateRegex(value, validateType.getRegVal(), validateType, key, result);
-                                break;
-                            default:// 其他正则校验(数字校验,时间校验,邮箱校验,手机号校验,url校验,ip地址校验,国内邮编校验,正则表达式校验)
-                                validateRegex(value, validateType.getRegexs(), validateType, key, result);
-                                break;
-                        }
-                        if (!RETURN_CODE.IS_OK.equals(result.getReturnCode())) {// 校验失败，返回
-                            return result;
-                        }
-                    } catch (Exception e) {
-                        System.err.println(e.getMessage());
+                value = valueObject.toString();
+                try {
+                    switch (validateType) {
+                        case required:// 必填字段校验
+                            isRequired(value, validateType, key, result);
+                            break;
+                        case length:// 长度校验
+                            validateLength(value, validateType.getRegVal(), validateType, key, result);
+                            break;
+                        case maxlength:// 长度最大值校验
+                            validateMaxlength(value, validateType.getRegVal(), validateType, key, result);
+                            break;
+                        case minlength:// 长度最小值校验
+                            validateMinlength(value, validateType.getRegVal(), validateType, key, result);
+                            break;
+                        case range:// 区间校验
+                            validateRange(value, validateType.getRegVal(), validateType, key, result);
+                            break;
+                        case idcard:// 证件号校验
+                            // 校验规则：15位通过 || 18位通过
+                            validateRegex(value, ValidateType.REGEX.IDCARD_15, validateType, key, result);
+                            if (!RETURN_CODE.IS_OK.equals(result.getReturnCode())) {
+                                validateRegex(value, ValidateType.REGEX.IDCARD_18, validateType, key, result);
+                            }
+                            break;
+                        case regex:// 正则表达式校验
+                            validateRegex(value, validateType.getRegVal(), validateType, key, result);
+                            break;
+                        default:// 其他正则校验(数字校验,时间校验,邮箱校验,手机号校验,url校验,ip地址校验,国内邮编校验,正则表达式校验)
+                            validateRegex(value, validateType.getRegexs(), validateType, key, result);
+                            break;
                     }
+                    if (!RETURN_CODE.IS_OK.equals(result.getReturnCode())) {// 校验失败，返回
+                        return result;
+                    }
+                } catch (Exception e) {
+                    System.err.println(e.getMessage());
                 }
+            }
         }
         return result;
 
@@ -152,10 +156,8 @@ public class InputValidInterceptor implements Interceptor {
     /**
      * 正则表达式校验
      *
-     * @param regex
-     *            正则表达式
-     * @param value
-     *            被校验的值
+     * @param regex 正则表达式
+     * @param value 被校验的值
      * @return 校验结果
      */
     private static boolean matchRegex(String value, String regex) {
@@ -167,11 +169,10 @@ public class InputValidInterceptor implements Interceptor {
     /**
      * 获取待校验的列表
      *
-     * @param key 配置的Key值,只供显示错误用
      * @param regexs 配置的校验列表
      * @return 规则列表
      */
-    public static List<ValidateType> getValidateTypes(String key, String regexs) {
+    public static List<ValidateType> getValidateTypes(String regexs) {
         List<ValidateType> typeList = new ArrayList<ValidateType>();
 
         // 配置为空,则不校验
@@ -210,10 +211,10 @@ public class InputValidInterceptor implements Interceptor {
     /**
      * 判断是否非空,如果value为空,则处理返回值
      *
-     * @param value 前台输入的值
-     * @param key 前台输入的参数名
+     * @param value        前台输入的值
+     * @param key          前台输入的参数名
      * @param validateType 校验规则
-     * @param result 校验返回值
+     * @param result       校验返回值
      */
     private static void isRequired(String value, ValidateType validateType,
                                    String key, ValidateResult result) {
@@ -253,7 +254,7 @@ public class InputValidInterceptor implements Interceptor {
                 result.setReturnMessage(getText(validateType.getErrMsg(), key, String.valueOf(length)));
             }
         } catch (Exception e) {
-            System.err.println("VALIDATE MAX LENGTH ERROR ! THE ERROR IS "+e.getMessage()+"!");
+            System.err.println("VALIDATE MAX LENGTH ERROR ! THE ERROR IS " + e.getMessage() + "!");
         }
     }
 
@@ -269,7 +270,7 @@ public class InputValidInterceptor implements Interceptor {
                 result.setReturnMessage(getText(validateType.getErrMsg(), key, String.valueOf(length)));
             }
         } catch (Exception e) {
-            System.err.println("VALIDATE MIN ERROR ! THE ERROR IS "+e.getMessage()+"!");
+            System.err.println("VALIDATE MIN ERROR ! THE ERROR IS " + e.getMessage() + "!");
         }
     }
 
@@ -280,7 +281,7 @@ public class InputValidInterceptor implements Interceptor {
                                       ValidateType validateType, String key, ValidateResult result) {
         final String regex = "^\\s*(\\(|\\[)\\s*((-?\\d+)(\\.\\d+)?)+\\s*~\\s*((-?\\d+)(\\.\\d+)?)+\\s*(\\)|\\])\\s*$";
         if (!matchRegex(scope, regex)) {// 校验配置的区间值格式是否正确
-            System.err.println("RANGE CONFIGURATION ERROR!! THE WRONG RANGE IS='" + scope+"'!!");
+            System.err.println("RANGE CONFIGURATION ERROR!! THE WRONG RANGE IS='" + scope + "'!!");
         }
         scope = scope.replaceAll(" ", "");//去空格
         // 取出配置的字段
@@ -289,7 +290,7 @@ public class InputValidInterceptor implements Interceptor {
         double min = Double.parseDouble(scopes[0].substring(1));
         double max = Double.parseDouble(scopes[1].substring(0, scopes[1].length() - 1));
         if (min > max) {//配置错误
-            System.err.println("RANGE CONFIGURATION ERROR!! THE WRONG RANGE IS='" + scope+"'!! MAXVALUE MUST GREATER THEN MINVALUE!!");
+            System.err.println("RANGE CONFIGURATION ERROR!! THE WRONG RANGE IS='" + scope + "'!! MAXVALUE MUST GREATER THEN MINVALUE!!");
         }
         String firstBrackets = String.valueOf(scopes[0].charAt(0));
 
@@ -300,20 +301,20 @@ public class InputValidInterceptor implements Interceptor {
                 System.err.println("输入的值必须大于最小值！");
             }
         } else if ("[".equals(firstBrackets)) {
-            if (value < min){
+            if (value < min) {
                 validateFlag = Boolean.FALSE;
                 System.err.println("输入的值必须大于等于最小值！");
             }
         }
 
         String lastBrackets = String.valueOf(scopes[1].charAt(scopes[1].length() - 1));
-        if (")".equals(lastBrackets) ) {// 和大数校验
-            if (value >= max){
+        if (")".equals(lastBrackets)) {// 和大数校验
+            if (value >= max) {
                 validateFlag = Boolean.FALSE;
                 System.err.println("输入的值必须小于最大值！");
             }
         } else if ("]".equals(lastBrackets)) {
-            if (value > max){
+            if (value > max) {
                 validateFlag = Boolean.FALSE;
                 System.err.println("输入的值必须小于等于最大值！");
             }
@@ -328,34 +329,34 @@ public class InputValidInterceptor implements Interceptor {
     /**
      * 根据规则验证传入的值是否正确(如果传入值为空，则不校验)
      *
-     * @param value 传入值
-     * @param regex 正则表达式
+     * @param value        传入值
+     * @param regex        正则表达式
      * @param validateType 校验类型
-     * @param key 前台输入的参数名
+     * @param key          前台输入的参数名
      * @return 验证结果
      */
     private static void validateRegex(String value, String regex,
-                                      ValidateType validateType,String key,ValidateResult result) {
+                                      ValidateType validateType, String key, ValidateResult result) {
         if (value == null || "".equals(value.trim())) {
             return;
         }
         if (!matchRegex(value, regex)) {// 校验失败,封装返回结果
             result.setReturnCode(RETURN_CODE.SYSTEM_ERROR);
-            result.setReturnMessage(getText(validateType.getErrMsg(),key));
+            result.setReturnMessage(getText(validateType.getErrMsg(), key));
         }
     }
 
     /**
-     *  拼接返回内容
+     * 拼接返回内容
      */
     private static String getText(String localeKey, String key) {
         return key + localeKey;
     }
 
     /**
-     *  拼接返回内容
+     * 拼接返回内容
      */
-    private static String getText(String localeKey, String key,String params) {
+    private static String getText(String localeKey, String key, String params) {
         return key + localeKey + params;
     }
 }
