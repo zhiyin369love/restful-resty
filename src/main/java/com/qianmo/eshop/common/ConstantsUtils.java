8c0dd45b14ba84092d842240e487b472e7f82396
fangxigang <fangxigang@ahqianmo.com>
2016-04-08 13:09:31 +0800

    更改图片上传目录

--- src/main/java/com/qianmo/eshop/common/ConstantsUtils.java
+++ src/main/java/com/qianmo/eshop/common/ConstantsUtils.java
@@ -165,11 +165,11 @@ public class ConstantsUtils {
 
 	/**************************图片存储目录*****************************/
 	/**图片存储总目录**/
-	public final static String PIC_DIR = "http://www.anz001.com/upload";
+	public final static String PIC_DIR = "/upload";
 	/**商品主图存储目录**/
-	public final static String GOODS_MAIN_PIC = "http://www.anz001.com/upload/goods/";
+	public final static String GOODS_MAIN_PIC = "/upload/goods/";
 	/**商品详情图片存储目录**/
-	public final static String GOODS_DETAIL_PIC = "http://www.anz001.com/upload/detail/";
+	public final static String GOODS_DETAIL_PIC = "/upload/detail/";
 	/******************************************************************/
 
 	/****************************分页*****************************/
